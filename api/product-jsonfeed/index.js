bb8b0a6bad31edaf3439a4d5e7ce41afa6c84216
Bret Comnes <bcomnes@gmail.com>
2019-08-03 11:51:59 +0200

    Write feed generation functions

---
+++ api/product-jsonfeed/index.js
@@ -0,0 +1,83 @@
+/* eslint-disable camelcase */
+const url = require('url')
+const jsonfeedToRSS = require('jsonfeed-to-rss')
+
+exports.getPurchace = getPurchace
+function getPurchace (data, purchaseId) {
+  return data.products.find(p => p['purchase_id'] === purchaseId)
+}
+
+exports.productsWithFileData = productsWithFileData
+function productsWithFileData (data) {
+  return data.products.filter(p => p['file_data'].length > 0)
+}
+
+exports.getProductPermalink = getProductPermalink
+function getProductPermalink (product) {
+  const downloadURL = product['file_data'][0].download_url
+  const u = new url.URL(downloadURL)
+  console.log(u.pathname.split('/'))
+  const permalinkId = u.pathname.split('/')[5]
+  return `https://gumroad.com/d/${permalinkId}`
+}
+
+const gumroadFaviconSvg = 'https://assets.gumroad.com/assets/logo-70cc6d4c5ab29be1bae97811585bc664524cd99897327ec47a67a76a6f69be91.svg'
+const gumroadFavicon = 'https://gumroad.com/favicon.ico'
+
+exports.getJsonfeed = getJsonfeed
+function getJsonfeed (data, opts = {}) {
+  const {
+    purchaseId,
+    feed_url
+  } = opts
+  const product = getPurchace(data, purchaseId)
+  if (!product) return null
+  const home_page_url = `https://gumroad.com/d/${purchaseId}`
+
+  const jsonfeed = {
+    version: 'https://jsonfeed.org/version/1',
+    title: product.name,
+    home_page_url,
+    feed_url,
+    description: product.description,
+    user_comment: `Feed generated and delivered by gumcast.com`,
+    icon: product.preview_url || gumroadFaviconSvg,
+    favicon: gumroadFaviconSvg,
+    author: {
+      name: product.creator_name,
+      avatar: product.preview_url
+    },
+    _itunes: {
+      expired: !product.file_data,
+      block: true
+      // new_feed_url, TODO: for refresh token?,
+    },
+    expired: !product.file_data,
+    items: product.file_data.map((item, i) => ({
+      id: item.id,
+      url: home_page_url,
+      title: item.name_displayable,
+      content_text: item.name_displayable,
+      image: product.preview_url,
+      banner_image: product.preview_url,
+      date_published: item.created_at,
+      attachments: [{
+        url: item.download_url,
+        mime_type: `${item.filegroup}/${item.filetype}`,
+        title: item.name_displayable,
+        size_in_bytes: item.size
+      }],
+      _itunes: {
+        episode: i
+      }
+    }))
+  }
+
+  return jsonfeed
+}
+
+exports.getRssFeed = getRssFeed
+function getRssFeed (data, opts = {}) {
+  const jf = getJsonfeed(data, opts)
+  return jsonfeedToRSS(jf)
+}
--- api/product-jsonfeed/index.test.js
+++ api/product-jsonfeed/index.test.js
@@ -1,6 +1,48 @@
 const test = require('tape')
 const data = require('./test-data.json')
+const { productsWithFileData, getPurchace, getProductPermalink, getJsonfeed, getRssFeed } = require('./index.js')
 
-test('get a json feed from raw data', t => {
+test('getProductsWithFiles', t => {
+  const productsWithFiles = productsWithFileData(data)
 
+  for (const product of productsWithFiles) {
+    t.true(product['file_data'].length > 0, `${product.name} has file_data`)
+  }
+  t.end()
+})
+
+test('Get specific purchase', t => {
+  const uberBook = getPurchace(data, '7vvS02eBjHXfPeUhKxjq8A==')
+  t.equal(uberBook.name, "Uber's Undoing eBook", 'retrieved correct product')
+  t.end()
+})
+
+test('Get permalink', t => {
+  const uberBook = getPurchace(data, '7vvS02eBjHXfPeUhKxjq8A==')
+  const permalink = getProductPermalink(uberBook)
+  t.equal(permalink, 'https://gumroad.com/d/redacted', 'retrieved correct permalink')
+  t.end()
+})
+
+test('Generate jsonfeed', t => {
+  const jf = getJsonfeed(data, {
+    purchaseId: '7vvS02eBjHXfPeUhKxjq8A==',
+    feed_url: `https://gumcast.com/feed.json?authToken=1234&refreshToken=1234&productId=1234`
+  })
+
+  console.dir(jf, {
+    colors: true,
+    depth: null
+  })
+  t.end()
+})
+
+test('Generate rss feed', t => {
+  const rss = getRssFeed(data, {
+    purchaseId: '7vvS02eBjHXfPeUhKxjq8A==',
+    feed_url: `https://gumcast.com/feed.rss?authToken=1234&refreshToken=1234&productId=1234`
+  })
+
+  console.log(rss)
+  t.end()
 })
