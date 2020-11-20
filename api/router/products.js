61982f49764a5ecb02a34ac2bed983db8abdf411
Bret Comnes <bcomnes@gmail.com>
2020-11-20 09:20:03 -0700

    Filter out older duplicate purchase data from products endpoint

--- api/router/products.js
+++ api/router/products.js
@@ -3,6 +3,7 @@ const { route } = require('p-connect')
 const parseurl = require('parseurl')
 const qs = require('qs')
 const { getPurchaces } = require('../gumroad-client.js')
+const get = require('lodash.get')
 
 const { apiErrorHandler, validationFailed, writeBody } = require('./helpers.js')
 
@@ -30,7 +31,30 @@ function products (cfg) {
         mobileApiUrl: cfg.mobileApiUrl
       })
 
-      return writeBody(req, res, JSON.stringify(purchasedItems))
+      const filteredProductData = { ...purchasedItems }
+      filteredProductData.products = filteredProductData.products.filter(p => Array.isArray(p.file_data))
+
+      const productPurchaseMap = {}
+
+      for (const p of filteredProductData.products) {
+        const purchaseArray = get(productPurchaseMap, p.unique_permalink) || []
+        purchaseArray.push(p)
+        productPurchaseMap[p.unique_permalink] = purchaseArray
+      }
+
+      for (const [uniqePermalink, purchaseArray] of Object.entries(productPurchaseMap)) {
+        productPurchaseMap[uniqePermalink] = purchaseArray.sort((a, b) => new Date(b.purchased_at) - new Date(a.purchased_at))
+      }
+
+      const latestPurchaseList = new Set()
+
+      for (const sortedPurchaseArray of Object.values(productPurchaseMap)) {
+        latestPurchaseList.add(sortedPurchaseArray[0].purchase_id)
+      }
+
+      filteredProductData.products = filteredProductData.products.filter(p => latestPurchaseList.has(p.purchase_id))
+
+      return writeBody(req, res, JSON.stringify(filteredProductData))
     } catch (e) {
       return apiErrorHandler(req, res, e)
     }
